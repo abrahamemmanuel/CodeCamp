baa6057971deed3a2584aebbf558fa36ee38bd68
Abraham Emmanuel <sundayemmanuelabraham@gmail.com>
2022-05-23 11:32:46 +0100

    create user model

---
+++ app/Http/Controllers/Api/V1/AuthController.js
@@ -0,0 +1,5 @@
+const Bootcamp = require('../../../../Models/User');
+const User = require('../../../../Models/Bootcamp');
+const ErrorResponse = require('../../../../Utils/errorResponse');
+const asyncHandler = require('../../../Middleware/async');
+
